a78827ca9c6c7634108f229407387664da8b635c
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2023-04-30 20:20:37 -0300

    fix: adicionado função de definir o título no componente de resultado de batalha

--- Scripts/BattleResultMenu.gd
+++ Scripts/BattleResultMenu.gd
@@ -1,5 +1,14 @@
-
 extends Node
+
+onready var title = $VBoxContainer2/CenterContainer/Label
+
+func set_title(win: bool):
+	if win:
+		title.text = "Victory Achieved!"
+		title.modulate = Color.green
+	else:
+		title.text = "Defeat!"
+		title.modulate = Color.red
 
 func _on_ContinueButton_pressed():
 	SceneTransition.change_scene("res://Scenes/SecondStage.tscn")
@@ -7,4 +16,3 @@ func _on_ContinueButton_pressed():
 func _ready():
 	var continue_button = get_node("VBoxContainer/ContinueButton")
 	continue_button.grab_focus()
-	
