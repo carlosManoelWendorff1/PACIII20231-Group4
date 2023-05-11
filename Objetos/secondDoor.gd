743de1fdb5e2fa93d3f51b17e50f619bb8e4f4a2
GustavoStorch <gustavokauan2002@hotmail.com>
2023-05-11 17:05:12 -0300

    ajuste de transições das portas

--- Objetos/secondDoor.gd
+++ Objetos/secondDoor.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 func _on_AreaPorta_area_entered(area):
-	SceneTransition.change_scene("res://Scenes/secondStage.tscn")
+	SceneTransition.change_scene("res://Scenes/secondStage2.tscn")
 	
 	print("colidiu com a porta") # Replace with function body.
